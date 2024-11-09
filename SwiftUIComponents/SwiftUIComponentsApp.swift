f3dc990139739430ebd50a6bfa5859a086d8d1ba
chengchao <chengchao60827@gmail.com>
2024-11-09 23:46:16 +0800

    Initial Commit

---
+++ SwiftUIComponents/SwiftUIComponentsApp.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUIComponentsApp.swift
+//  SwiftUIComponents
+//
+//  Created by Chao Cheng on 11/9/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftUIComponentsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SwiftUIComponentsTests/SwiftUIComponentsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentsTests.swift
+//  SwiftUIComponentsTests
+//
+//  Created by Chao Cheng on 11/9/24.
+//
+
+import Testing
+@testable import SwiftUIComponents
+
+struct SwiftUIComponentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
