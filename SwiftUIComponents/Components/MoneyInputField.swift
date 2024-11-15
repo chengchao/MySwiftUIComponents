c9afec73d3c40a54aa9e004c44e5c02d2818e499
chengchao <chengchao60827@gmail.com>
2024-11-15 22:16:52 +0800

    fix: fix the bug when digits are more than 3

--- SwiftUIComponents/Components/MoneyInputField.swift
+++ SwiftUIComponents/Components/MoneyInputField.swift
@@ -10,6 +10,13 @@ struct MoneyInputField: View {
   @Binding var amountInCents: Int
   @State private var formattedAmountString = "0"
 
+  private static let numberFormatter: NumberFormatter = {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .decimal
+    formatter.minimumFractionDigits = 0
+    return formatter
+  }()
+
   var body: some View {
     VStack {
       TextField("Enter amount", text: $formattedAmountString)
@@ -20,11 +27,7 @@ struct MoneyInputField: View {
         }
         .onChange(of: formattedAmountString) { oldValue, newValue in
           formattedAmountString = formatNumberString(oldValue: oldValue, newValue: newValue)
-          if let number = Double(formattedAmountString) {
-            amountInCents = Int(number * 100)
-          } else {
-            amountInCents = 0
-          }
+          amountInCents = Int((Double(sanitizeInput(formattedAmountString)) ?? 0) * 100)
         }
     }
   }
@@ -57,12 +60,10 @@ struct MoneyInputField: View {
     }
   }
 
-  private let nonNumericCharactersRegex = try! NSRegularExpression(pattern: "[^0-9.]", options: [])
-
   private func sanitizeInput(_ input: String) -> String {
-    let range = NSRange(location: 0, length: input.utf16.count)
-    return nonNumericCharactersRegex.stringByReplacingMatches(
-      in: input, options: [], range: range, withTemplate: "")
+    // Remove any invalid characters (only digits, `.` are allowed)
+    let allowedCharacters = CharacterSet(charactersIn: "0123456789.")
+    return String(input.unicodeScalars.filter { allowedCharacters.contains($0) })
   }
 
   private func truncateDecimalPlaces(_ value: String) -> String {
@@ -74,13 +75,6 @@ struct MoneyInputField: View {
     return value
   }
 
-  private static let numberFormatter: NumberFormatter = {
-    let formatter = NumberFormatter()
-    formatter.numberStyle = .decimal
-    formatter.minimumFractionDigits = 0
-    return formatter
-  }()
-
   private func formatNumber(_ number: Double) -> String {
     let result =
       MoneyInputField.numberFormatter.string(from: NSNumber(value: number)) ?? String(number)
@@ -90,5 +84,7 @@ struct MoneyInputField: View {
 
 #Preview {
   @Previewable @State var amountInCents: Int = 0
-  return MoneyInputField(amountInCents: $amountInCents)
+  return MoneyInputField(amountInCents: $amountInCents).onChange(of: amountInCents) { oldValue, newValue in
+    print("Amount changed from \(oldValue) to \(newValue)")
+  }
 }
